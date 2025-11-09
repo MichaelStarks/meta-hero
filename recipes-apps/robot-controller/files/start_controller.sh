19c865221ed5f855d55e665c26c38a4facfb14f1
Michael Starks <starksmichael224@gmail.com>
2025-11-08 19:28:41 -0700

    attempting auto start

---
+++ recipes-apps/robot-controller/files/start_controller.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+export ROBOTID=1
+export COLCON_CURRENT_PREFIX=/home/root/install
+# source /opt/ros/jazzy/setup.sh
+# source install/setup.sh
+
+# while true
+# do
+#     echo "Starting ROS Node"
+#     ros2 launch robot_controller.launch.py
+#     echo "Robot Controller died"
+# done
